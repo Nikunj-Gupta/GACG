930877bc64d10efbf38834a6959d71a26c43bf9c
Nikunj Gupta <ng2531@nyu.edu>
2024-08-21 20:08:47 -0700

    added maco envs

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,5 +1,9 @@
 from functools import partial
 from smac.env import MultiAgentEnv, StarCraft2Env
+from .gather import GatherEnv
+from .disperse import DisperseEnv
+from .pursuit import PursuitEnv
+from .hallway import HallwayEnv
 import sys
 import os
 
@@ -8,8 +12,12 @@ def env_fn(env, **kwargs) -> MultiAgentEnv:
 
 REGISTRY = {}
 REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
-
+REGISTRY["gather"] = partial(env_fn, env=GatherEnv)
+REGISTRY["disperse"] = partial(env_fn, env=DisperseEnv)
+REGISTRY["pursuit"] = partial(env_fn, env=PursuitEnv)
+REGISTRY["hallway"] = partial(env_fn, env=HallwayEnv)
 
 if sys.platform == "linux":
-    os.environ.setdefault("SC2PATH",
-                          os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
+    os.environ.setdefault("SC2PATH", "~/StarCraftII") 
+
+
