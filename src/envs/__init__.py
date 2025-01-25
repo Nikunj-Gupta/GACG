d266c38cb622dd733548d2a49527eeacc58cad53
Nikunj Gupta <ng2531@nyu.edu>
2025-01-25 04:22:35 -0800

    pogema envs

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,9 +1,10 @@
 from functools import partial
-from smac.env import MultiAgentEnv, StarCraft2Env
+from smacv2.env import MultiAgentEnv, StarCraft2Env, StarCraftCapabilityEnvWrapper
 from .gather import GatherEnv
 from .disperse import DisperseEnv
 from .pursuit import PursuitEnv
 from .hallway import HallwayEnv
+from .pogema import PyMarlPogema
 import sys
 import os
 
@@ -12,10 +13,12 @@ def env_fn(env, **kwargs) -> MultiAgentEnv:
 
 REGISTRY = {}
 REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
+REGISTRY["sc2wrapped"] = partial(env_fn, env=StarCraftCapabilityEnvWrapper)
 REGISTRY["gather"] = partial(env_fn, env=GatherEnv)
 REGISTRY["disperse"] = partial(env_fn, env=DisperseEnv)
 REGISTRY["pursuit"] = partial(env_fn, env=PursuitEnv)
 REGISTRY["hallway"] = partial(env_fn, env=HallwayEnv)
+REGISTRY["pogema"] = partial(env_fn, env=PyMarlPogema) 
 
 if sys.platform == "linux":
     os.environ.setdefault("SC2PATH", "~/StarCraftII") 
