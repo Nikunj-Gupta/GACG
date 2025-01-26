eef3e5f6a5016ea747f253fe27e17d66f94cd2ab
nikunj-gupta <ng2531@nyu.edu>
2025-01-25 19:48:28 -0800

    qtran

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -2,6 +2,7 @@ from .q_learner import QLearner
 from .dcg_learner import DCGLearner
 from .gacg_learner import GroupQLearner
 from .vast_learner import VastQLearner
+from .qtran_learner import QLearner as QTranLearner
 
 REGISTRY = {}
 
@@ -10,3 +11,5 @@ REGISTRY["dcg_learner"] = DCGLearner
 
 REGISTRY["gacg_learner"] = GroupQLearner
 REGISTRY["vast_learner"] = VastQLearner
+
+REGISTRY["qtran_learner"] = QTranLearner 
