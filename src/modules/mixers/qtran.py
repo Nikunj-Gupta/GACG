50407613e48ade9108b837021531d1e6d0e4a602
nikunj-gupta <ng2531@nyu.edu>
2025-02-08 18:31:44 -0800

    attention-based graphs

--- src/modules/mixers/qtran.py
+++ src/modules/mixers/qtran.py
@@ -2,6 +2,7 @@ import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from torch_geometric.nn import GATConv 
 from components.tgat_module import TGANMARL 
 from components.tgat_graph import NeighborFinder 
 
@@ -17,6 +18,11 @@ class QTranBase(nn.Module):
         self.arch = self.args.qtran_arch # QTran architecture
 
         self.embed_dim = args.mixing_embed_dim
+        self.edge_index = self.get_edge_index(N=self.n_agents)
+        self.gat = GATConv(self.args.rnn_hidden_dim, self.args.rnn_hidden_dim, heads=1, concat=False) 
+
+        ngh_finder = NeighborFinder(adj_list=[[] for _ in range(self.n_agents + 1)]) 
+        self.tgan = TGANMARL(ngh_finder, self.args.rnn_hidden_dim) 
 
         # Q(s,u)
         if self.arch == "coma_critic":
@@ -67,6 +73,20 @@ class QTranBase(nn.Module):
                                                  nn.Linear(ae_input, ae_input))
         else:
             assert False
+    
+    def get_edge_index(self, N, type="full"): # need an initial graph construction 
+        if type == "line": 
+            edges = [[i, i + 1] for i in range(N - 1)]  # # arrange agents in a line 
+        elif type == "full": 
+            edges = [[(j, i + j + 1) for i in range(N - j - 1)] for j in range(N - 1)]
+            edges = [e for l in edges for e in l] 
+        elif type == 'cycle':    # arrange agents in a circle
+            edges = [(i, i + 1) for i in range(N - 1)] + [(N - 1, 0)] 
+        elif type == 'star':     # arrange all agents in a star around agent 0
+            edges = [(0, i + 1) for i in range(N - 1)] 
+        edge_index = th.tensor(edges).T # # arrange agents in a line   
+        # return edge_index.to(self.device) 
+        return edge_index
 
     def forward(self, batch, hidden_states, actions=None):
         bs = batch.batch_size
@@ -91,17 +111,21 @@ class QTranBase(nn.Module):
                 actions = actions.reshape(bs * ts, self.n_agents, self.n_actions)
             
             hidden_states = hidden_states.reshape(-1, self.args.rnn_hidden_dim) 
-            train_src_l = np.random.choice(range(1, self.args.n_agents + 1), bs * ts * self.n_agents).tolist() 
-            train_dst_l = np.random.choice(range(1, self.args.n_agents + 1), bs * ts * self.n_agents).tolist() 
+
+            hidden_states, (edges, weights) = self.gat(hidden_states, edge_index=self.edge_index, return_attention_weights=True) 
+
+            train_src_l = edges[0][:-self.edge_index.shape[1]].tolist() 
+            train_dst_l = edges[1][:-self.edge_index.shape[1]].tolist() 
             train_e_idx_l = list(range(1, bs * ts * self.n_agents + 1)) 
-            train_ts_l = np.random.choice(range(1, ts + 1), bs * ts * self.n_agents).tolist() 
-            adj_list = [[] for _ in range(self.n_agents + 1)] 
+            train_ts_l = [v for v in list(range(1, ts + 1)) for _ in range(self.n_agents)]*bs 
+
+            adj_list = [[] for _ in range(bs * ts * self.n_agents + 1)] 
             for src, dst, eidx, ts in zip(train_src_l, train_dst_l, train_e_idx_l, train_ts_l): 
                 adj_list[src].append((dst, eidx, ts))
                 adj_list[dst].append((src, eidx, ts)) 
             ngh_finder = NeighborFinder(adj_list) 
-            tgan = TGANMARL(ngh_finder, hidden_states, hidden_states) 
-            hidden_states = tgan.forward(src_idx_l=np.array(train_src_l), cut_time_l=np.array(train_ts_l)) 
+            self.tgan.ngh_finder = ngh_finder 
+            hidden_states = self.tgan.forward(n_feat_th=hidden_states, src_idx_l=np.array(train_src_l), cut_time_l=np.array(train_ts_l)) 
 
             hidden_states = hidden_states.reshape(-1, self.n_agents, self.args.rnn_hidden_dim)
             agent_state_action_input = th.cat([hidden_states, actions], dim=2)
