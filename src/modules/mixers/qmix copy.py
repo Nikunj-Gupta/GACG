3608c3c861f4bc7836fba06ef655373c9cf9f4e9
Your Name[Da <you@example.com>
2025-04-14 17:41:12 -0700

    fix

--- src/modules/mixers/qmix copy.py
+++ src/modules/mixers/qmix copy.py
@@ -223,13 +223,13 @@ class QMixer(nn.Module):
         
         hidden_flat = hidden_states.view(bs, ts, -1)  # shape: (bs, ts, n * hidden_state_dim)
         
-        combined_states = torch.cat([states, hidden_flat], dim=-1)  # shape: (bs, ts, state_dim + n*hidden_state_dim)
+        combined_states = th.cat([states, hidden_flat], dim=-1)  # shape: (bs, ts, state_dim + n*hidden_state_dim)
         
         # First layer
         w1 = th.abs(self.hyper_w_1(states))
         b1 = self.hyper_b_1(states)
         w1 = w1.view(-1, self.n_agents, self.embed_dim)
-        b1 = b1.view(-1, 1, self.embed_dim)
+        b1 = b1.view(-1, 1, self.embed_dim)    
         hidden = F.elu(th.bmm(agent_qs, w1) + b1)
         # Second layer
         w_final = th.abs(self.hyper_w_final(states))
